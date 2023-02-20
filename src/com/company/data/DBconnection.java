8820f6cbcee39575e96df03d33b7d4d8133cfb98
samatbelentbay <test@mail.ru>
2023-02-20 12:23:39 +0600

    finish

--- src/com/company/data/DBconnection.java
+++ src/com/company/data/DBconnection.java
@@ -6,7 +6,7 @@ public class DBconnection {
 
     public static Connection connection(){
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/marketplace", "postgres", "1079");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/marketplace", "postgres", "Nursat12#");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
